6cc93c25ebe59ccec7e6033a600190a6f6276bde
CunningFoxBVE <bovdavladislav@gmail.com>
2023-12-19 12:25:59 +0200

    yes

---
+++ pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:1111@tcp(127.0.0.1:3306)/gotestdb?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
